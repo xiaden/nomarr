5e3ed73392e5695a184362b4d89359dc48a9ffb8
Lucian Hardy <applesmycat@aol.com>
2026-02-11 18:03:55 +1000

    chore: frontend build

---
+++ nomarr/public_html/assets/useConfirmDialog-BAcnlcyY.js
@@ -0,0 +1,1 @@
+import{Fn as e,Ln as t}from"./index-TiZNNpCT.js";var n=t(e(),1);function r(){let[e,t]=(0,n.useState)(!1),[r,i]=(0,n.useState)({title:``,message:``}),[a,o]=(0,n.useState)(null);return{isOpen:e,options:r,confirm:e=>(i(e),t(!0),new Promise(e=>{o(()=>e)})),handleConfirm:()=>{t(!1),a?.(!0),o(null)},handleCancel:()=>{t(!1),a?.(!1),o(null)}}}export{r as t};
