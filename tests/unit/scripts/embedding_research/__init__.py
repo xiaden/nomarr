db2650f65d05764aa3c6b8bfc89d2f90dca6b1b9
Lucian Hardy <applesmycat@aol.com>
2026-05-22 08:39:20 +1000

    test: delete embedding research tests (research scripts don't need unit tests)

--- tests/unit/scripts/embedding_research/__init__.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-from pkgutil import extend_path
-
-__path__ = extend_path(__path__, __name__)
-__path__.append(str(Path(__file__).resolve().parents[4] / "scripts" / "embedding_research"))
